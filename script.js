378638e46371c002fae55e64b9e74ef9d53412bc
alexgadd <alex.gadd2@gmail.com>
2017-03-10 11:45:48 -0500

    added true/false

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ console.log("Welcome to HANGMAN!")
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
  'p', 'q', 'r', 'x', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var words = ['javascript', 'rails', 'variable', 'array', 'function', 'jquery',
- 'github', 'terminal', 'homework', 'project'];
+var word = "javascript";
 
-var randNum = Math.floor(Math.random() * words.length);
 
 $("#alphabet").on("click", function($event) {
-var userGuess = $($event.target).attr('id');
-alert(userGuess);
+	
+	var userGuess = $($event.target).attr('id');
+	alert("The character you guessed was: " + userGuess);
+
+	var correct = word.includes(userGuess); 
+	alert("Your guess was " + correct);
 });
 
 }
